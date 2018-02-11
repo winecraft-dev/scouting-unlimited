78835f1a109f91ac1e0b99ca3d1a8e231b3a4997
newbie958 <newbie9598@gmail.com>
2018-02-10 22:43:39 -0600

    Schedule worked on, need to figure out table

--- views/ScheduleView.php
+++ views/ScheduleView.php
@@ -4,9 +4,38 @@ class ScheduleView implements View
 {
   public function render()
   { ?>
-    <div class="schedule-content">
+    <div class="page-section">
       <a href="/?c=login&do=logout">Logout</a>
       <a href="/?p=dataentry">Data Entry</a>
+      <table class=ScheduleTable>
+        <tr class="top-bar">
+
+					<th><b>Match</b></th>
+
+					<th><b>Red 1</b></th>
+
+					<th><b>Red 2</b></th>
+
+					<th><b>Red 3</b></th>
+
+					<th><b>Blue 1</b></th>
+
+					<th><b>Blue 2</b></th>
+
+					<th><b>Blue 3</b></th>
+
+					<th><b>Time</b></th>
+
+				</tr>
+      <?php
+      $schedule = (new MatchScheduleDatabaseModel())->getMatches();
+      foreach($schedule as $match)
+      {
+        ?><td><?php$match->matchnumber;?></td>
+
+      }
+      ?>
+    </table>
     </div>
   <?php }
 }
